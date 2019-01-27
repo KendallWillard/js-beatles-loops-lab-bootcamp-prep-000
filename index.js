f4e8bc8c5a2734d1aebf73c36c5d07f6620ae466
kendall willard <kendallwillard@hotmail.com>
2019-01-27 23:21:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments) {
-  var alpha = new Array();
+  var alpha = []
   for( var i = 0; i <= musicians.length - 1; i++) {
     alpha.push(musicians[i] + " plays " + instruments[i])
   }
