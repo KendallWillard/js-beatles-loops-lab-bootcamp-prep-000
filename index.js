ead6a0b1c66ca48b7729770b9186623a6eb6df63
kendall willard <kendallwillard@hotmail.com>
2019-01-27 23:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians, instruments) {
   var alpha = new Array();
-  for( var i = 0; i < musicians.length - 1; i++) {
+  for( var i = 0; i <= musicians.length - 1; i++) {
     alpha.push(musicians[i] + " plays " + instruments[i])
   }
   return alpha;
